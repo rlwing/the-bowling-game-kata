44a59d461e00e22bf2f626ad3eca6c496ce408b1
rlwing <rob@wingjr.com>
2020-08-03 12:34:40 -0700

    Copy acceptance criteria

--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -1,5 +1,12 @@
 package com.galvanize.bowling;
 
 public class GameTest {
-
+//    ### Acceptance Criteria
+//    1. create a new game
+//    1. can record rolling of ball
+//    1. all (20) gutters
+//    1. all (20) ones
+//    1. one spare, one roll with pins down, and the rest (17) gutters
+//    1. one strike, two rolls with pins down, and the rest (16) gutters
+//    1. perfect game - 10 strikes + two 10s (strikes)
 }
