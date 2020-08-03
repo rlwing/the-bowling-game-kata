94d2297d73e2cbeb0d8a2f3733506fe89c9b3691
rlwing <rob@wingjr.com>
2020-08-03 14:24:16 -0700

    Test 3 - Can score gutter game

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -2,6 +2,9 @@ package com.galvanize.bowling;
 
 public class Game {
     public void roll(int pinsDown) {
+    }
 
+    public int score() {
+        return 0;
     }
 }
--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -3,6 +3,7 @@ package com.galvanize.bowling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GameTest {
@@ -22,13 +23,20 @@ public class GameTest {
 
 
 //    1. can record rolling of ball
-
     @Test
     void canRollBall() {
         game.roll(0);
     }
 
 //    1. all (20) gutters
+    @Test
+    void canScoreGutterGame() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+
 //    1. all (20) ones
 //    1. one spare, one roll with pins down, and the rest (17) gutters
 //    1. one strike, two rolls with pins down, and the rest (16) gutters
