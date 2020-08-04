f8a6139cd8e670a410cc46a51c9d25b07e96f3ad
rlwing <rob@wingjr.com>
2020-08-04 10:37:31 -0700

    Finished - Cleaned up

--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -4,32 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GameTest {
-//    ### Acceptance Criteria
-//    1. create a new game
-//    @Test
-//    void canCreateNewGame() {
-//        Game game = new Game();
-//        assertNotNull(game);
-//    }
-    // Refactor to remove redundancy
+
     Game game;
+
     @BeforeEach
     void setUp() {
         this.game = new Game();
     }
 
-
-//    2. can record rolling of ball
-//No longer necessary, roll is called in test 3
-//    @Test
-//    void canRollBall() {
-//        game.roll(0);
-//    }
-
-//    3. all (20) gutters
     @Test
     void canScoreGutterGame() {
         for (int i = 0; i < 20; i++) {
@@ -38,7 +22,6 @@ public class GameTest {
         assertEquals(0, game.score());
     }
 
-//    4. all (20) ones
     @Test
     void canScoreAllOnes() {
         for (int i = 0; i < 20; i++) {
@@ -47,7 +30,6 @@ public class GameTest {
         assertEquals(20, game.score());
     }
 
-//    5. one spare, one roll with pins down, and the rest (17) gutters
     @Test
     void canScore_oneSpare_roll5_restGutters() {
         game.roll(5);
@@ -59,7 +41,6 @@ public class GameTest {
         assertEquals(20, game.score());
     }
 
-//    6. one strike, two rolls with pins down, and the rest (16) gutters
     @Test
     void canScore_oneStrike_roll5_roll5_restGutters() {
         game.roll(10);
@@ -71,7 +52,6 @@ public class GameTest {
         assertEquals(30, game.score());
     }
 
-//    7. perfect game - 10 strikes + two 10s (strikes)
     @Test
     void canScore_perfectGame() {
         for (int i = 0; i < 12; i++) {
