270cd120fab62366e1f8f41b6c7eda8e63d2a538
rlwing <rob@wingjr.com>
2020-08-04 12:40:44 -0700

    Remove Hello World

--- src/main/java/com/galvanize/bowling/App.java
+++ src/main/java/com/galvanize/bowling/App.java
@@ -4,11 +4,4 @@
 package com.galvanize.bowling;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
 }
